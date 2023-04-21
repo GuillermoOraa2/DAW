412c0dce7171b8d0e4156a4d57d4d3cbf65d8dc4
GuillermoOraa1 <retaziles@hotmail.com>
2023-04-21 15:58:05 +0200

    with translations

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import i18nBackend from "i18next-http-backend";
 
-const getCurrentHost = 'https://dcge6afp39vqf.cloudfront.net/';
+const getCurrentHost =window.location.href;
 i18n.use(i18nBackend)
     .use(initReactI18next)
     .init({
